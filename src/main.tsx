e12ec1f4a2eca1da89597fa1c8cccce586f1e7fd
thisiskushal31 <73896226+thisiskushal31@users.noreply.github.com>
2025-07-02 22:36:25 +0530

    fix: Remove unused HashRouter import from main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 
 // Main application entry point
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
