71a3709b5ffc9f7c2772b557fad38247db6d2e3c
thisiskushal31 <73896226+thisiskushal31@users.noreply.github.com>
2025-07-02 18:14:36 +0530

    feat: Add postbuild script to copy index.html for 404 handling and wrap App component with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-
 // Main application entry point
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
 // Render the React application
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+<HashRouter>
+<App />
+</HashRouter>
+);
